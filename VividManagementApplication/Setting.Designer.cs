42105c3e062b3338b4c6ba115e9cf08b1200f079
Z <731jerry@gmail.com>
2015-03-25 17:22:53 +0800

    设置界面初步修改

--- VividManagementApplication/Setting.Designer.cs
+++ VividManagementApplication/Setting.Designer.cs
@@ -28,21 +28,289 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Setting));
+            this.tbClient6 = new System.Windows.Forms.TextBox();
+            this.tbClient1 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label39 = new System.Windows.Forms.Label();
+            this.SaveButton = new ControlExs.QQButton();
+            this.qqButton1 = new ControlExs.QQButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tbClient4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // tbClient6
+            // 
+            this.tbClient6.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.tbClient6.Location = new System.Drawing.Point(120, 120);
+            this.tbClient6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbClient6.Name = "tbClient6";
+            this.tbClient6.Size = new System.Drawing.Size(436, 26);
+            this.tbClient6.TabIndex = 124;
+            // 
+            // tbClient1
+            // 
+            this.tbClient1.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.tbClient1.Location = new System.Drawing.Point(120, 18);
+            this.tbClient1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbClient1.Name = "tbClient1";
+            this.tbClient1.Size = new System.Drawing.Size(436, 26);
+            this.tbClient1.TabIndex = 122;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label12.Location = new System.Drawing.Point(25, 123);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(85, 20);
+            this.label12.TabIndex = 116;
+            this.label12.Text = "*公司地址：";
+            // 
+            // label39
+            // 
+            this.label39.AutoSize = true;
+            this.label39.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label39.Location = new System.Drawing.Point(25, 21);
+            this.label39.Name = "label39";
+            this.label39.Size = new System.Drawing.Size(85, 20);
+            this.label39.TabIndex = 114;
+            this.label39.Text = "*公司名称：";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.SaveButton.Image = ((System.Drawing.Image)(resources.GetObject("SaveButton.Image")));
+            this.SaveButton.Location = new System.Drawing.Point(41, 465);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(91, 30);
+            this.SaveButton.TabIndex = 127;
+            this.SaveButton.Text = "保存";
+            this.SaveButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // qqButton1
+            // 
+            this.qqButton1.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.qqButton1.Image = ((System.Drawing.Image)(resources.GetObject("qqButton1.Image")));
+            this.qqButton1.Location = new System.Drawing.Point(465, 465);
+            this.qqButton1.Name = "qqButton1";
+            this.qqButton1.Size = new System.Drawing.Size(91, 30);
+            this.qqButton1.TabIndex = 128;
+            this.qqButton1.Text = "取消";
+            this.qqButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.qqButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.textBox1.Location = new System.Drawing.Point(394, 52);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 26);
+            this.textBox1.TabIndex = 130;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label1.Location = new System.Drawing.Point(303, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 129;
+            this.label1.Text = "*联系电话：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.textBox2.Location = new System.Drawing.Point(394, 154);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 26);
+            this.textBox2.TabIndex = 134;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label2.Location = new System.Drawing.Point(303, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 133;
+            this.label2.Text = "*开户账号：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.textBox3.Location = new System.Drawing.Point(120, 154);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(177, 26);
+            this.textBox3.TabIndex = 132;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label3.Location = new System.Drawing.Point(25, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 131;
+            this.label3.Text = "*开户行：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.textBox4.Location = new System.Drawing.Point(394, 86);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(162, 26);
+            this.textBox4.TabIndex = 138;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label4.Location = new System.Drawing.Point(303, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 137;
+            this.label4.Text = "*传真电话：";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.textBox5.Location = new System.Drawing.Point(120, 86);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(177, 26);
+            this.textBox5.TabIndex = 136;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label5.Location = new System.Drawing.Point(25, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 135;
+            this.label5.Text = "*邮箱地址：";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label13.Location = new System.Drawing.Point(25, 55);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(71, 20);
+            this.label13.TabIndex = 115;
+            this.label13.Text = "*联系人：";
+            // 
+            // tbClient4
+            // 
+            this.tbClient4.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.tbClient4.Location = new System.Drawing.Point(120, 52);
+            this.tbClient4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbClient4.Name = "tbClient4";
+            this.tbClient4.Size = new System.Drawing.Size(177, 26);
+            this.tbClient4.TabIndex = 123;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.textBox6.Location = new System.Drawing.Point(120, 188);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(177, 26);
+            this.textBox6.TabIndex = 140;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 10.5F);
+            this.label6.Location = new System.Drawing.Point(25, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 20);
+            this.label6.TabIndex = 139;
+            this.label6.Text = "*公司法人：";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(29, 264);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(527, 100);
+            this.panel1.TabIndex = 141;
             // 
             // Setting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(547, 459);
+            this.ClientSize = new System.Drawing.Size(597, 523);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.qqButton1);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.tbClient6);
+            this.Controls.Add(this.tbClient4);
+            this.Controls.Add(this.tbClient1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label39);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Setting";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "用户设置";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbClient6;
+        private System.Windows.Forms.TextBox tbClient1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label39;
+        private ControlExs.QQButton SaveButton;
+        private ControlExs.QQButton qqButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox tbClient4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
     }
 }
